7b7947a82c3981ba6a8c764f5e2b0b4fa235cb7f
SarahGrevy <65611758+SarahGrevy@users.noreply.github.com>
2020-05-21 12:41:26 -0400

    Coughing_woman_03

--- models.py
+++ models.py
@@ -6,21 +6,37 @@ from otree.api import (
 
 doc = ''
 class Constants(BaseConstants):
-    name_in_url = 'Resell_hand_sanitizer_02'
+    name_in_url = 'Coughing_woman_03'
     players_per_group = 2
     num_rounds = 1
-    my_constant = ()
+    my_constant = 0
+    my_constant2 = 0
+    my_constant3 = ()
+    List = ()
+    my_constant4 = ''
 class Subsession(BaseSubsession):
-    pass
+    my_field = models.CurrencyField()
 class Group(BaseGroup):
+    discussion1 = models.FloatField()
+    my_field = models.FloatField()
+    my_field2 = models.StringField(initial='')
+    my_field3 = models.StringField()
+
     def live_slider(self, id_in_group, package):
         broadcast = {"slider_value": package["slider_value"], "disable": package["disable"]}
         return {0: broadcast}
+              
+              
 class Player(BasePlayer):
-    PartnerAgreement = models.StringField(choices=[['Strongly disagree', 'Strongly disagree'], ['Somewhat disagree ', 'Somewhat disagree '], ['Disagree ', 'Disagree '], ['Neither agree nor disagree', 'Neither agree nor disagree'], ['Somewhat agree', 'Somewhat agree'], ['Agree', 'Agree']], label='I think that my partner and I are on the same wavelength with regard to our judgments of the video', widget=widgets.RadioSelectHorizontal)
+    Name = models.StringField(label='What is your name?')
+    Age = models.IntegerField(label='What is your age?')
+    PartnerAgreement = models.StringField(choices=[['Strongly disagree', 'Strongly disagree'], ['Disagree', 'Disagree'], ['Somewhat disagree', 'Somewhat disagree'], ['Neither agree nor disagree', 'Neither agree nor disagree'], ['Somewhat agree', 'Somewhat agree'], ['Agree', 'Agree']], label='I think that my partner and I are on the same wavelength with regard to our judgments of the video', widget=widgets.RadioSelectHorizontal)
     judgement1 = models.FloatField()
     certainty1 = models.FloatField()
     judgement2 = models.FloatField()
     certainty2 = models.FloatField()
     judgement3 = models.FloatField()
     certainty3 = models.FloatField()
+    mutual_judgment = models.IntegerField()
+    def my_method(self):
+        "live_slider"
--- pages.py
+++ pages.py
@@ -4,17 +4,17 @@ from ._builtin import Page, WaitPage
 from .models import Constants
 
 class Stimuli(Page):
-    form_model = 'player'
-class Judgement1(Page):
+    form_model = 'group'
+class Judgement_1(Page):
     form_model = 'player'
     form_fields = ['judgement1']
 class Certainty_1(Page):
     form_model = 'player'
     form_fields = ['certainty1']
-class MyWaitPage_1(WaitPage):
+class MyWaitPage_2(WaitPage):
     pass
 class Discussion_1(Page):
-    form_model = 'group'
+    form_model = 'player'
     live_method = "live_slider"
 class Wavelength(Page):
     form_model = 'player'
@@ -25,7 +25,7 @@ class Judgement_2(Page):
 class Certainty_2(Page):
     form_model = 'player'
     form_fields = ['certainty2']
-class MyWaitPage_2(WaitPage):
+class MyWaitPage_3(WaitPage):
     pass
 class Stimuli_Rewatch(Page):
     form_model = 'player'
@@ -35,6 +35,6 @@ class Judgement_3(Page):
 class Certainty_3(Page):
     form_model = 'player'
     form_fields = ['certainty3']
-class MyWaitPage_3(WaitPage):
+class MyWaitPage_4(WaitPage):
     pass
-page_sequence = [Stimuli, Judgement1, Certainty_1, MyWaitPage_1, Discussion_1, Wavelength, Judgement_2, Certainty_2, MyWaitPage_2, Stimuli_Rewatch, Judgement_3, Certainty_3, MyWaitPage_3]
+page_sequence = [Stimuli, Judgement_1, Certainty_1, MyWaitPage_2, Discussion_1, Wavelength, Judgement_2, Certainty_2, MyWaitPage_3, Stimuli_Rewatch, Judgement_3, Certainty_3, MyWaitPage_4]
