bf43fa9524ad991c41c79f8f17cfbb1f3db67467
SarahGrevy <65611758+SarahGrevy@users.noreply.github.com>
2020-05-21 13:35:42 -0400

    Woolies_toilet_paper_00

--- models.py
+++ models.py
@@ -6,31 +6,18 @@ from otree.api import (
 
 doc = ''
 class Constants(BaseConstants):
-    name_in_url = 'Coughing_woman_03'
+    name_in_url = 'Woolies_toilet_paper_00'
     players_per_group = 2
     num_rounds = 1
-    my_constant = 0
-    my_constant2 = 0
-    my_constant3 = ()
-    List = ()
-    my_constant4 = ''
+    my_constant = ()
 class Subsession(BaseSubsession):
-    my_field = models.CurrencyField()
+    pass
 class Group(BaseGroup):
-    discussion1 = models.FloatField()
-    my_field = models.FloatField()
-    my_field2 = models.StringField(initial='')
-    my_field3 = models.StringField()
-
     def live_slider(self, id_in_group, package):
         broadcast = {"slider_value": package["slider_value"], "disable": package["disable"]}
         return {0: broadcast}
-              
-              
 class Player(BasePlayer):
-    Name = models.StringField(label='What is your name?')
-    Age = models.IntegerField(label='What is your age?')
-    PartnerAgreement = models.StringField(choices=[['Strongly disagree', 'Strongly disagree'], ['Disagree', 'Disagree'], ['Somewhat disagree', 'Somewhat disagree'], ['Neither agree nor disagree', 'Neither agree nor disagree'], ['Somewhat agree', 'Somewhat agree'], ['Agree', 'Agree']], label='I think that my partner and I are on the same wavelength with regard to our judgments of the video', widget=widgets.RadioSelectHorizontal)
+    PartnerAgreement = models.StringField(choices=[['Strongly disagree', 'Strongly disagree'], ['Somewhat disagree ', 'Somewhat disagree '], ['Disagree ', 'Disagree '], ['Neither agree nor disagree', 'Neither agree nor disagree'], ['Somewhat agree', 'Somewhat agree'], ['Agree', 'Agree']], label='I think that my partner and I are on the same wavelength with regard to our judgments of the video', widget=widgets.RadioSelectHorizontal)
     judgement1 = models.FloatField()
     certainty1 = models.FloatField()
     judgement2 = models.FloatField()
